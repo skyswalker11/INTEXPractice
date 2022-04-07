7d8afb55505d0086c9424c573d1f689945c8bdc6
Spencer Walker <swalker2172@gmail.com>
2022-04-07 16:04:19 -0600

    Add project files.

---
+++ INTEXPractice/Controllers/InferenceController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using INTEXPractice.Models;
+
+namespace aspnetcore.Controllers
+{ 
+    public class InferenceController : Controller
+    {
+        private InferenceSession _session;
+
+        public InferenceController(InferenceSession session)
+        {
+            _session = session;
+        }
+
+        [HttpGet]
+        public IActionResult EnterData()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Score(InsuranceData data)
+        {
+            var result = _session.Run(new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("float_input", data.AsTensor())
+            });
+            Tensor<float> score = result.First().AsTensor<float>();
+            var prediction = new Prediction { PredictedValue = score.First() };
+            result.Dispose();
+            return View("Score", prediction);
+        }
+    }
+}
---
+++ INTEXPractice/Models/InsuranceData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.ML.OnnxRuntime.Tensors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace INTEXPractice.Models
+{
+    public class InsuranceData
+    {
+        //public int InsuranceId { get; set; }
+        public float Year { get; set; }
+        public float Month { get; set; }
+        public float Day { get; set; }
+        public float Milepoint { get; set; }
+        public float Intersection_Related { get; set; }
+        public float  Teenage_Driver_Involved { get; set; }
+        public float  Night_Dark_Condition { get; set; }
+        public float Single_Vehicle { get; set; }
+
+        public float Crash_Id { get; set; }
+
+        public Tensor<float> AsTensor()
+        {
+            float[] data = new float[]
+            {
+                Year, Month, Day, Milepoint, Intersection_Related, Teenage_Driver_Involved, Night_Dark_Condition, Single_Vehicle
+            };
+            int[] dimensions = new int[] { 1, 8 };
+            return new DenseTensor<float>(data, dimensions);
+        }
+    }
+}
